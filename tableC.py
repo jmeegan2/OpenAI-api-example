d52304108e9885db5292a36019697238276509b8
jmeegan2 <jmeegan8@gmail.com>
2022-12-15 03:10:40 -0500

    different criteria for response

--- tableC.py
+++ tableC.py
@@ -2,12 +2,12 @@ import openai
 import html
 
 # Set your OpenAI API key
-openai.api_key = "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"
+openai.api_key = "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"
 
 # Call the Completion API
 response = openai.Completion.create(
   model="text-davinci-003",
-  prompt="A two-column spreadsheet of top science fiction movies and the year of release:\n\nTitle |  Year of release",
+  prompt="A two-column spreadsheet of top historical movies and the year of release:\n\nTitle |  Year of release",
   temperature=0.5,
   max_tokens=60,
   top_p=1,
